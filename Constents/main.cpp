90bdb290c1195e76e630f6b84b927e65d252a8d0
VowcicefskiJ <96920791+VowcicefskiJ@users.noreply.github.com>
2023-10-22 19:38:53 -0400

    Add files via upload

---
+++ Constents/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout<< "Hello, welcome to Frank's Carpet Cleaning Service" <<endl;
+    
+    int small_rooms {0};
+    cout<< "\nHow many small rooms would you like cleaned?";
+    cin>> small_rooms;
+    
+    int large_rooms {0};
+    cout<< "\nHow many large rooms would you like cleaned?"; 
+    cin>> large_rooms;
+    
+    const double price_per_small_room {25.0};
+    const double price_per_large_room {35.0};
+    
+    const double sales_tax {0.06};
+    const int estimate_expiry {30};//days
+    
+    
+    cout<< "\nEstimate for carpet cleaning service"<<endl;
+    cout<< "Number_of_small_rooms"<<small_rooms<<endl;
+    cout<< "Number_of_large_rooms"<<large_rooms<<endl;
+    
+    cout<< "Price per room:$"<<price_per_small_room<<endl;
+    cout<< "Price per room:$"<<price_per_large_room<<endl;
+    
+    cout<< "Cost: $"<<(price_per_small_room* small_rooms)
+                     +(price_per_large_room* large_rooms)<<endl;
+                    
+    cout<< "Tax: $"<<((price_per_small_room* small_rooms)
+                    +(price_per_large_room* large_rooms))* sales_tax<<endl;
+    cout<< "==========================================="<<endl;
+    cout<< "Total estimate: $"
+            <<((price_per_small_room* small_rooms)+ (price_per_large_room* large_rooms))
+            +(((price_per_small_room* small_rooms)+ (price_per_large_room* large_rooms))* sales_tax)
+            <<endl;
+
+    cout<< "This estimate is valid for " << estimate_expiry << " days" <<endl;
+    
+
+    cout<<endl;
+    return 0;
+}
